8c02b5cac865a03d333507ee324ec1975f839bfc
michaelpaliy <michael.paliy@niceactimize.com>
2021-05-11 19:36:22 +0300

    embrace spring flyway

--- postgres-persistence/src/test/java/com/netflix/conductor/postgres/util/PostgresDAOTestUtil.java
+++ postgres-persistence/src/test/java/com/netflix/conductor/postgres/util/PostgresDAOTestUtil.java
@@ -34,45 +34,25 @@ import static org.mockito.Mockito.when;
 
 public class PostgresDAOTestUtil {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PostgresDAOTestUtil.class);
     private final HikariDataSource dataSource;
     private final PostgresProperties properties = mock(PostgresProperties.class);
     private final ObjectMapper objectMapper;
-    private final DataSource initializationDataSource;
 
-    public PostgresDAOTestUtil(PostgreSQLContainer postgreSQLContainer, ObjectMapper objectMapper, String dbName) {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setURL(postgreSQLContainer.getJdbcUrl());
-        dataSource.setDatabaseName(dbName);
-        dataSource.setUser(postgreSQLContainer.getUsername());
-        dataSource.setPassword(postgreSQLContainer.getPassword());
-        this.initializationDataSource = dataSource;
+    public PostgresDAOTestUtil(PostgreSQLContainer<?> postgreSQLContainer, ObjectMapper objectMapper) {
 
         this.objectMapper = objectMapper;
 
-        when(properties.getJdbcUrl()).thenReturn(postgreSQLContainer.getJdbcUrl());
-        when(properties.getJdbcUsername()).thenReturn(postgreSQLContainer.getUsername());
-        when(properties.getJdbcPassword()).thenReturn(postgreSQLContainer.getPassword());
-        when(properties.isFlywayEnabled()).thenReturn(true);
-        when(properties.getTaskDefCacheRefreshInterval()).thenReturn(Duration.ofSeconds(60));
-
-        this.dataSource = getDataSource(properties);
-    }
-
-    private HikariDataSource getDataSource(PostgresProperties properties) {
-
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(properties.getJdbcUrl());
-        dataSource.setUsername(properties.getJdbcUsername());
-        dataSource.setPassword(properties.getJdbcPassword());
+        this.dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(postgreSQLContainer.getJdbcUrl());
+        dataSource.setUsername(postgreSQLContainer.getUsername());
+        dataSource.setPassword(postgreSQLContainer.getPassword());
         dataSource.setAutoCommit(false);
-
         // Prevent DB from getting exhausted during rapid testing
         dataSource.setMaximumPoolSize(8);
 
-        flywayMigrate(dataSource);
+        when(properties.getTaskDefCacheRefreshInterval()).thenReturn(Duration.ofSeconds(60));
 
-        return dataSource;
+        flywayMigrate(dataSource);
     }
 
     private void flywayMigrate(DataSource dataSource) {
@@ -98,26 +78,5 @@ public class PostgresDAOTestUtil {
         return objectMapper;
     }
 
-    public static void dropDb(DataSource ds, String dbName) {
-        exec(ds, dbName, "DROP", "IF EXISTS");
-    }
 
-    private static void exec(DataSource ds, String dbName, String prefix, String suffix) {
-
-        try (Connection connection = ds.getConnection()) {
-            String stmt = String.format("%s DATABASE %s %s", prefix, suffix, dbName);
-            try (PreparedStatement ps = connection.prepareStatement(stmt)) {
-                ps.executeUpdate();
-            }
-        } catch (SQLException ex) {
-            LOGGER.error(ex.getMessage(), ex);
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public void resetAllData() {
-        LOGGER.info("Resetting data for test");
-        dropDb(initializationDataSource, "conductor");
-        flywayMigrate(dataSource);
-    }
 }
